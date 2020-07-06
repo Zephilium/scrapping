0bcffb696a732fbb7c5b2728859cf04a3456d2a7
Zephilium <wuuuuush@gmail.com>
2020-07-06 19:53:10 +0700

    bab 1 part 1

---
+++ kompas_scraper.py
@@ -0,0 +1,17 @@
+import requests
+from bs4 import BeautifulSoup
+
+url = requests.get("https://www.kompas.com/")
+soup = BeautifulSoup(url.text, "html.parser")
+
+popular_area = soup.find(attrs={"class": "most ga--most mt1 clearfix"})
+
+titles = popular_area.find_all(attrs={"class": "most__title"})
+
+views = popular_area.find_all(attrs={"class": "most__read"})
+
+z = 0
+
+for i in titles:
+    print(f"\n{i.text}\n{views[z].text}")
+    z+=1
---
+++ kumparan_scraper.py
@@ -0,0 +1,15 @@
+import requests
+from bs4 import BeautifulSoup
+
+url = requests.get("https://kumparan.com/trending")
+soup = BeautifulSoup(url.text, "html.parser")
+
+popular_area = soup.find(attrs={"class" : "TrendingView__TrendingListViewContainer-sc-11x2h62-0 jKmbrh"})
+titles = popular_area.find_all(attrs={"class" : "TextBoxweb__StyledTextBox-n41hy7-0 feePYD"})
+
+for x in titles:
+    print(x.text)
+
+images = popular_area.find_all(attrs={"class" : "Thumbnailweb__ThumbnailContainer-xodrtt-2 goTQKp"})
+
+# print(images)
